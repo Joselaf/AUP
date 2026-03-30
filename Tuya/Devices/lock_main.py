5c9f963d7cc46b2f050834e6b5b54633b683bf53
Luis <luis.jlaf@hotmail.com>
2026-03-30 17:05:31 +0100

    all functions

--- Tuya/Devices/lock_main.py
+++ Tuya/Devices/lock_main.py
@@ -17,3 +17,45 @@ class lock_main:
         self.p2p_id = self.dps('108')
         self.video_flip = self.dps('115')
         self.night_mode = self.dps('116')
+
+    def get_feed_status():
+        return self.feed_status
+    
+    def get_battery():
+        return self.battery
+    
+    def get_pir_switch():
+        return self.pir_switch
+
+    def get_pir_sensitivity():
+        return self.pir_sensitivity
+    
+    def set_pir_sensitivity(sense):
+        self.device.set_dps(sense, '104')
+        self.pir_sensitivity = sense
+
+    def get_sd_status():
+        return self.sd_status
+    
+    def get_p2p_id():
+        return self.p2p_id
+    
+    def get_video_flip():
+        return self.video_flip
+
+    def set_video_flip(value):
+        self.device.set_dps(value, '115')
+        self.video_flip = value
+
+    
+    def get_night_mode():
+        return self.night_mode
+
+    def set_nighgt_mode(value):
+        self.device.set_dps(value, '116')
+        self.night_mode = value
+
+
+
+
+
--- Tuya/Devices/smart_plug.py
+++ Tuya/Devices/smart_plug.py
@@ -14,4 +14,44 @@ class smart_plug:
         self.countdown = self.dps('9')
         self.relay_status = self.dps('38')
         self.child_lock = self.dps('40')
-        
+
+
+    def get_id():
+        return self.id
+
+    def get_name():
+        return self.name
+    
+    def get_state():
+        return self.state
+
+    def Toogle():
+        new_state = not self.state
+        self.device.set_dps('1', new_state)
+        self.state = new_state
+        return(new_state)
+    
+    def set_countdown(value):        
+        self.device.set_dps('9', value)
+        self.countdown = value
+
+    def get_countdown():        
+        return self.countdown
+    
+    def set_relay_status(value):
+        self.device.set_dps('38', value)
+        self.relay_status = value
+
+    def get_relay_status():        
+        return self.relay_status
+    
+    def get_child_lock():
+        return self.child_lock
+    
+    def set_child_lock(value):
+        self.device.set_dps('40', value)
+        self.child_lock = value
+
+
+
+
